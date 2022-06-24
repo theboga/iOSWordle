c8f0c0d6e1f762f4869795db29f4551dd50307b6
Ignacio Bogarin <the_boga@hotmail.com>
2022-06-24 00:02:17 -0500

    Initial Commit

---
+++ iOSWordle/iOSWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSWordleApp.swift
+//  iOSWordle
+//
+//  Created by Ignacio Bogarin on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
